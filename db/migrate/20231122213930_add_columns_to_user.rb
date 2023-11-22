23c81908692823215ffbf107ce19d7f8da053b11
martin-creator <martinlubowa@outlook.com>
2023-11-23 01:05:16 +0300

    Updated User model

---
+++ db/migrate/20231122213930_add_columns_to_user.rb
@@ -0,0 +1,9 @@
+class AddColumnsToUser < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :full_name, :string
+    add_column :users, :from, :string
+    add_column :users, :about, :text
+    add_column :users, :language, :string
+    add_column :users, :status, :boolean
+  end
+end
