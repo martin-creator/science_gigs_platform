027470b692000157ba550302d9ef25ea4c9572c7
martin-creator <martinlubowa@outlook.com>
2023-11-23 01:05:02 +0300

    Created user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
+  get 'users/new'
+  get 'users/create'
+  get 'users/edit'
+  get 'users/update'
+  get 'users/destroy'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   # root "articles#index"
 
   root 'home#index'
-
-  # devise_for :users
 
 end
