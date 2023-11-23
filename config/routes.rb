45c516a061513d1c5e0ff8b7cd25f38c5332e9fe
martin-creator <martinlubowa@outlook.com>
2023-11-24 00:42:30 +0300

    Renamed routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,16 @@ Rails.application.routes.draw do
   get 'users/edit'
   get 'users/update'
   get 'users/destroy'
-  devise_for :users
+  # devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   # root "articles#index"
 
   root 'home#index'
+
+  devise_for :users,
+              path: '',
+              path_names: {sign_in: 'login', sign_out: 'logout', edit: 'profile', sign_up: 'register'},
+              controllers: { registrations: 'registrations'}
 
 end
