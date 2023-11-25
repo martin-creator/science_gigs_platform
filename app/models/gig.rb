50cb7eadc83435be8615f8ab754b5b3499502fae
martin-creator <martinlubowa@outlook.com>
2023-11-25 22:12:54 +0300

    Updated gig page

---
+++ app/models/gig.rb
@@ -0,0 +1,18 @@
+class Gig < ApplicationRecord
+  belongs_to :user
+  belongs_to :category
+
+  has_many :pricings
+  has_many_attached :photos
+  has_rich_text :description
+  # has_many :orders
+  # has_many :reviews
+
+  accepts_nested_attributes_for :pricings
+
+  validates :title, presence: {message: 'cannot be blank'} 
+
+  # def average_rating
+  #   reviews.count == 0 ? 0 : reviews.average(:stars).round(1)
+  # end
+end
